cdd227006903a444f8292b976a8692a186a12c8f
vrizas <vrizasizza@gmail.com>
2021-05-06 15:32:36 +0700

    Menambahkan file home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Piew</title>
+	<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>	
+	<!-- Navbar -->
+	<div class="nav-container">
+		<nav>
+			<h1 class="piew-logo">Piew!</h1>
+			<div class="nav-wrapper">
+				<ul>
+					<li><a href="#">TENTANG KAMI</a></li>
+					<li>
+						<form action="" method="POST">
+							<input class="seach-box-1" type="text" placeholder="Makanan Favoritmu">
+							<input class="search-box-2" type="text" placeholder="Lokasimu">
+							<button type="submit"><i class='bx bx-search'></i></button>
+						</form>
+					</li>
+					<li><a href="#">TULIS REVIEW</a></li>
+					<li><a href="#"><i class='bx bx-log-in'></i>MASUK</a></li>
+				</ul>
+			</div>
+		</nav>	
+	</div>
+	<!-- Akhir Navbar -->
+	
+	<!-- Header -->
+	<div class="header-container">
+		<header>
+			<div class="header-wrapper">
+				<h1>TEMUKAN</h1>
+				<h4>makanan enak tersembunyi di sekitarmu</h4>
+			</div>
+		</header>
+	</div>
+	<!-- Akhir Header -->
+
+	<!-- Konten Rekomendasi -->
+	<div class="content-container">
+		<div class="content-pilihan-kami">
+			<h3><a href="#">PILIHAN KAMI</a></h3>
+			<div class="img-wrapper-1">
+				<p><a href="#">Ayam Nelongso</a></p>
+				<a href="#"><img src="" alt="Ayam Nelongso"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-2">
+				<p><a href="#">MCD Suhat</a></p>
+				<a href="#"><img src="" alt="MCD Suhat"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-3">
+				<p><a href="#">Holy Cow</a></p>
+				<a href="#"><img src="" alt="Holy Cow"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-4">
+				<p><a href="#">Buah-buahan Bu Basri</a></p>
+				<a href=""><img src="" alt="Buah-buahan Bu Basri"></a>
+				<div class="rating">
+					<a href="">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<a href="#">Lihat Lebih Banyak</a>
+		</div>
+		<div class="content-masakan-padang">
+			<h3><a href="#">MASAKAN PADANG</a></h3>
+			<div class="img-wrapper-1">
+				<p><a href="#">Nasi Padang Pak Basri</a></p>
+				<a href="#"><img src="" alt="Nasi Padang Pak Basri"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-2">
+				<p><a href="#">Nasi Padang Sederhana</a></p>
+				<a href="#"><img src="" alt="Nasi Padang Sederhana"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-3">
+				<p><a href="#">Masakan Padang Dinoyo</a></p>
+				<a href="#"><img src="" alt="Masakan Padang Dinoyo"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<div class="img-wrapper-4">
+				<p><a href="#">Warung Padang Murah</a></p>
+				<a href="#"><img src="" alt="Warung Padang Murah"></a>
+				<div class="rating">
+					<a href="#">
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<i class='bx bxs-star' ></i>
+						<div class="rating-angka">4.0</div>
+					</a>
+				</div>
+			</div>
+			<a href="#">Lihat Lebih Banyak</a>
+		</div>
+	</div>
+	<!-- Akhir Konten Rekomendasi -->
+
+	<!-- Footer -->
+	<div class="footer-container">
+		<footer>
+			<div class="sosmed">
+				<i class='bx bxl-instagram'></i>
+				<i class='bx bxl-facebook-square'></i>
+				<i class='bx bxl-twitter' ></i>
+			</div>
+			<p><i class='bx bx-copyright' ></i> 2021 By Weject.</p>
+		</footer>
+	</div>
+	<!-- Akhir Footer -->
+	
+</body>
+</html>
