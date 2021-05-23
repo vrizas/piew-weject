57ebcc8fc319d0ee983d62c9da8c9251cfaa8a76
Arifwira <taros476@Gmail.com>
2021-05-23 15:24:41 +0700

    Mengubah tampilan halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,31 +20,30 @@
 </head>
 
 <body>
+
+
 	<!-- Navbar -->
 	<div class="nav-container">
 		<nav>
-		<a href="/"><img src="{{ asset('img/home/logo.svg') }}" class="logo"></a>
+			<img src="{{ asset('img/logo.png') }}" class="logo">
 			<div class="nav-menu">
 				<ul>
 					<li><a href="#">TENTANG KAMI</a></li>
 					<li><a href="#">TULIS REVIEW</a></li>
 					@if(session()->has('login'))
-						<li class='akun'>
-							@if(strlen(Auth::user()->name)>7)
-							<button class='drop'><i class='bx bxs-user-circle'></i> {{ substr(strip_tags(Auth::user()->name),0,7) }} <i class='bx bxs-chevron-down'></i></button>
-							@else
-							<button class='drop'><i class='bx bxs-user-circle'></i> {{ Auth::user()->name }} <i class='bx bxs-chevron-down'></i></button>
-							@endif
-							<div class="drop-content">
-								<form id="logout-form" action="{{ route('logout') }}" method="POST">
-									@csrf
-									<button type="submit"><i class='bx bx-log-out' ></i> Keluar</button>
-								</form>
-							</div>
-						</li>
+					<li class='akun'>
+						<button class='drop'><i class='bx bxs-user-circle'></i> {{ Auth::user()->name }} <i class='bx bxs-chevron-down'></i></button>
+						<div class="drop-content">
+							<form id="logout-form" action="{{ route('logout') }}" method="POST">
+								@csrf
+								<button style="font-size:.96rem;opacity:0;cursor:default;">{{ Auth::user()->name }}</button><br>
+								<button type="submit"><i class='bx bx-log-out'></i> Keluar</button>
+							</form>
+						</div>
+					</li>
 					@else
-						<li><a href="login"><i class='bx bx-log-in'></i> MASUK</a></li>
-						<li><a href="register"></i><i class='bx bxs-user-plus'></i> DAFTAR</a></li>
+					<li><a href="login"><i class='bx bx-log-in'></i> MASUK</a></li>
+					<li><a href="register"></i><i class='bx bxs-user-plus'></i> DAFTAR</a></li>
 					@endif
 				</ul>
 			</div>
@@ -57,12 +56,12 @@
 		<header>
 			<h1>TEMUKAN</h1>
 			<h4>makanan enak tersembunyi di sekitarmu</h4>
-			<form action="{{url('/search')}}" method="GET" role="search">
+			<form class="cari" action="{{url('/search')}}" method="GET" role="search">
 				@csrf
-				<input type="search" class="ket ket-1" placeholder="Cari" disabled>
-				<input class="search-box-1" name="kategori" type="text" placeholder="restoran,warung,makanan.." autocomplete="off">
+				<input type="text" class="ket ket-1" placeholder="Cari" disabled>
+				<input class="search-box-1" name="kategori" type="text" placeholder="restoran,warung,makanan..">
 				<input type="text" class="ket ket-2" placeholder="Lokasi" disabled>
-				<input class="search-box-2" name="lokasi" type="text" value="Malang, Jawa Timur" autocomplete="off">
+				<input class="search-box-2" name="lokasi" type="text" placeholder="Malang,Jawa Timur">
 				<button type="submit"><i class='bx bx-search'></i></button>
 			</form>
 		</header>
@@ -79,18 +78,18 @@
 			<h3>PILIHAN KAMI</h3>
 			<div class="content-pilihan-kami-wrapper">
 				@foreach($restaurants as $i => $restaurant)
-				
-				<div class="img-wrapper-{{ $i+1 }}">
+
+				<div class="img-wrapper-{{ $i }}">
 					<!-- Ambil dari Database -->
 					<p><a href="#" class="nama-resto">{{$restaurant->nama}}</a></p>
-					<a href="#"><img src="{{asset('img/bisnis_images')}}/{{$restaurant->image}}" alt="{{$restaurant->nama}}"></a>
-					<div class="rating rating-ke-{{$i+1}}">
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<div class="rating-angka rating-angka-ke-{{$i+1}}">{{$restaurant->rating}}</div>
+					<a class="kotak" href="#"><img src="{{asset('img/bisnis_images')}}/{{$restaurant->image}}" alt="{{$restaurant->nama}}"></a>
+					<div class="rating">
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<div class="rating-angka">{{$restaurant->rating}}</div>
 						<!-- ------------------- -->
 					</div>
 				</div>
@@ -102,17 +101,17 @@
 			<h3>MASAKAN PADANG</h3>
 			<div class="content-random-wrapper">
 				@foreach($restaurants as $i => $restaurant)
-				<div class="img-wrapper-{{ $i+1 }}">
+				<div class="img-wrapper-{{ $i }}">
 					<!-- Ambil dari Database -->
 					<p><a href="#" class="nama-resto">{{$restaurant->nama}}</a></p>
 					<a href="#"><img src="{{asset('img/bisnis_images')}}/{{$restaurant->image}}" alt="{{$restaurant->nama}}"></a>
-					<div class="rating rating-ke-{{$i+1}}">
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<i class='bx bx-star'></i>
-						<div class="rating-angka rating-angka-ke-{{$i+1}}">{{$restaurant->rating}}</div>
+					<div class="rating">
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<i class='bx bxs-star'></i>
+						<div class="rating-angka">{{$restaurant->rating}}</div>
 						<!-- ------------------- -->
 					</div>
 				</div>
@@ -126,18 +125,18 @@
 	<!-- Footer -->
 	<div class="footer-container">
 		<footer>
-			<div class="sosmed">
-				<i class='bx bxl-instagram'></i>
-				<i class='bx bxl-facebook-square'></i>
-				<i class='bx bxl-twitter'></i>
+			<div class="bawah">
+				<div class="sosmed">
+					<i class='bx bxl-instagram'></i>
+					<i class='bx bxl-facebook-square'></i>
+					<i class='bx bxl-twitter'></i>
+				</div>
+				<p><i class='bx bx-copyright'></i> 2021 By Weject.</p>
 			</div>
-			<p><i class='bx bx-copyright'></i> 2021 By Weject.</p>
 		</footer>
 	</div>
 	<!-- Akhir Footer -->
-
-<script src="{{ asset('js/akun.js') }}"></script>
-<script src="{{ asset('js/home.js') }}"></script>
+	<script src="{{ asset('js/home.js') }}"></script>
 </body>
 
 </html>
